37b31eacc3790493675034d4f75eede199c3b8ff
onepopina <anyarasp@gmail.com>
2023-04-12 22:23:42 +0200

    add propTypes

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -1,3 +1,4 @@
+import PropTypes from 'prop-types';
 import { TransactionTableBody } from './TransactionTableBody/TransactionTableBody';
 import { TransactionTableHeader } from './TransactionTableHeader/TransactionTableHeader';
 import css from './TransactionHistory.module.css';
@@ -9,4 +10,15 @@ export const TransactionHistory = ({ items }) => {
       <TransactionTableBody items={items} />
     </table>
   );
+};
+
+TransactionHistory.propTypes = {
+  transactions: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      type: PropTypes.string.isRequired,
+      amount: PropTypes.string.isRequired,
+      currency: PropTypes.string.isRequired,
+    })
+  ).isRequired,
 };
